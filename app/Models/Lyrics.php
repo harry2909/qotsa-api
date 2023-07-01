507a99fa6d5c0c9d57bd9e0d6de19aeaf74bd093
Harry Preston <47326455+harry2909@users.noreply.github.com>
2023-07-01 23:34:19 +0100

    add relationships

--- app/Models/Lyrics.php
+++ app/Models/Lyrics.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Lyrics extends Model
 {
@@ -15,4 +16,9 @@ class Lyrics extends Model
         'lyrics',
         'album_id',
     ];
+
+    public function song(): BelongsTo
+    {
+        return $this->belongsTo(Song::class);
+    }
 }
